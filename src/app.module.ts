e40dbbc272f38eb75c78a312882b23b20d338a4a
Prachi Pradiprao Rakhonde <prachi.rakhonde@acefone.com>
2025-01-23 16:49:35 +0530

    tasks module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './database/database.module';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { JwtModule } from '@nestjs/jwt';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
 
 @Module({
-  imports: [DatabaseModule,UserModule,AuthModule,JwtModule.register({
-      secret: 'your-secret-key', // Or use environment variable for secret
-      signOptions: { expiresIn: '1h' }, // Token expiry
-    }), 
-    ConfigModule.forRoot({isGlobal:true}),   
-  ],
-  controllers: [AppController,AuthController],
-  providers: [AppService,AuthService],
+  imports:[],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as session from 'express-session';
-import * as passport from 'passport';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin : "http://localhost:3000",
-    methods : "GET,POST,DELETE,PUT,PATCH",
-    credentials : true
-  })
-
-  app.use(
-    session({
-      secret: 'your-secret-key', // Replace with your secret
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-          httpOnly: true,
-          secure: false, // Set to `true` in production with HTTPS
-          maxAge: 3600000, // 1 hour
-      },
-    }),
-  );
-
-  app.use(passport.initialize())
-  app.use(passport.session())
-  
-  app.setGlobalPrefix('api');
-
   await app.listen(process.env.PORT ?? 5000);
   console.log(`Application is running on: http://localhost:5000`);
 
