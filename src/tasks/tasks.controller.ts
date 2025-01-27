82c93a52f28182f6af4736ff30f71dc8f12ca147
Prachi Pradiprao Rakhonde <prachi.rakhonde@acefone.com>
2025-01-27 11:03:57 +0530

    changes

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,14 +1,57 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Post, Body, Param, Delete, Patch, Query } from "@nestjs/common";
 import { TasksService } from "./tasks.service";
-import { Tasks } from "./tasks.model";
+import { Tasks, TaskStatus } from "./tasks.model";
+import { CreateTaskDto } from "./dto/create-tasks.dto";
+import { TaskFilterDto } from "./dto/get-tasks-filterDto";
+import { UpdateTaskStatusDto } from "./dto/update-task-status.dto";
 
 @Controller('tasks')
 export class TaskController {
     constructor(private tasksService : TasksService){}
     
     @Get()
-    getAllTasks() : Tasks[]{
-       return this.tasksService.getAllTasks()
+    getTasks(@Query() filterDto : TaskFilterDto) : Tasks[] {
+        if(Object.keys(filterDto).length){
+            return this.tasksService.getTaskByfilter(filterDto);
+        }
+        else{
+            return this.tasksService.getAllTasks()
+        }
+    }
+   
+
+    @Get('/:id')
+    getTaskById (@Param('id') id:string) : Tasks{
+        return this.tasksService.getTaskById(id);
+    }
+
+    @Post()
+    createTask(@Body() createTaskDto : CreateTaskDto) : Tasks{
+        return this.tasksService.createTask(createTaskDto)    
+    }
+
+    
+
+    // @Post()
+    // without using DTO
+    // createTask(@Body('title') title:string, @Body('description') description : string) : Tasks{
+    //     return this.tasksService.createTask(title,description)    
+    // }
+
+    @Patch('/:id/status')
+    updateTaskById(@Param('id') id : string, @Body() updateTaskStatusDto : UpdateTaskStatusDto ) : Tasks{
+        const {status} = updateTaskStatusDto
+        return this.tasksService.updateTaskById(id,status)
+    }
+
+    @Delete('/:id')
+    deleteTask(@Param('id') id:string) : void{
+       return this.tasksService.deleteTaskById(id)
     }
 
 }
+
+
+/*
+TaskDto already contains the particular fields which define the structure of incoming data
+*/
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,11 +1,69 @@
-import { Injectable } from "@nestjs/common";
-import { Tasks } from "./tasks.model";
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { Tasks, TaskStatus } from "./tasks.model";
+import {v4 as uuid} from 'uuid'
+import { CreateTaskDto } from "./dto/create-tasks.dto";
+import { TaskFilterDto } from "./dto/get-tasks-filterDto";
 
 @Injectable()
 export class TasksService {
     private tasks : Tasks[] = []; 
     
-    getAllTasks() : Tasks[]{
+    getAllTasks() : Tasks[] {
         return this.tasks;
     }
+
+    getTaskByfilter(filterDto:TaskFilterDto) : Tasks[]{
+        const {status,search} = filterDto;
+
+        let tasks = this.getAllTasks();
+        if(status){
+            tasks = tasks.filter((task)=> task.status === status)
+        }
+
+        if(search){
+            tasks = tasks.filter((task)=>{
+                if(task.title.includes(search) || task.description.includes(search)){
+                    return true;
+                }
+
+                return false;
+            })
+        }
+        return tasks
+
+    }
+
+    getTaskById(id : string) : Tasks{
+        let foundTask = this.tasks.find((task)=> task.id === id ) // if function doesnot have curly braces block, you can skip the return statement
+        if(!foundTask){
+            throw new NotFoundException(`Task with ${id} not found`)
+        }
+        
+        return foundTask
+    }
+
+    createTask(createTaskDto : CreateTaskDto) : Tasks{
+        let {title,description} = createTaskDto
+        let task : Tasks = {
+            id : uuid(),
+            title,
+            description,
+            status : TaskStatus.OPEN
+        }
+
+        this.tasks.push(task)
+        return task;
+    }
+
+    updateTaskById(id:string,status:TaskStatus) {
+        let getTask = this.getTaskById(id);
+        getTask.status = status
+        return getTask;
+    }
+
+    deleteTaskById(id:string) : void{
+        let found = this.getTaskById(id);
+        this.tasks = this.tasks.filter((task)=>task.id !== found.id)
+    }
+
 }
