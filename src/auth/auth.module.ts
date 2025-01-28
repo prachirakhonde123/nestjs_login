1dc91b5c23361caa9808bff3855c8070bd5cdc50
Prachi Pradiprao Rakhonde <prachi.rakhonde@acefone.com>
2025-01-28 18:48:43 +0530

    changes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,15 +7,25 @@ import { User } from "./user.entity";
 import { PassportModule } from "@nestjs/passport";
 import { JwtModule } from "@nestjs/jwt";
 import { JwtStrategy } from "./jwt.strategy";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
     imports : [
+        ConfigModule,
         PassportModule.register({defaultStrategy : 'jwt'}),
-        JwtModule.register({    // this module provide jwt service and we import it in authservice
-            secret : 'helloworld',
-            signOptions : {
-                expiresIn : 3600
-            }
+        JwtModule.registerAsync({    // this module provide jwt service and we import it in authservice
+            // secret : 'helloworld',
+            // signOptions : {
+            //     expiresIn : 3600
+            // }
+            imports:[ConfigModule],
+            inject:[ConfigService],
+            useFactory:async (configService : ConfigService)=>({
+                secret : configService.get('JWT_SECRET'),
+                signOptions : {
+                    expiresIn : 3600
+                }
+            })
         }),
         TypeOrmModule.forFeature([User])
     ],
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -5,15 +5,17 @@ import { Repository } from "typeorm";
 import { User } from "./user.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import { JwtPayload } from "./jwt-payload.interface";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy){
     constructor(
         @InjectRepository(User)
-        private userRepository : Repository<User>
+        private userRepository : Repository<User>,
+        private configService : ConfigService
     ){
         super({
-            secretOrKey : 'helloworld',
+            secretOrKey : configService.get('JWT_SECRET'),
             jwtFromRequest : ExtractJwt.fromAuthHeaderAsBearerToken()
         })
     }
